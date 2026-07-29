07e4dc5315c05389a985b4dae99b52f05a042b25
sonnanlo2125-a11y <sonnanlo2125@gmail.com>
2026-07-29 09:47:52 +0900

    Delete voice_processing directory
    딜리트

--- voice_processing/voice_processing/test_mic.py
+++
@@ -1,8 +0,0 @@
-import pyaudio
-p = pyaudio.PyAudio()
-
-print("--- 사용 가능한 입력 장치 목록 ---")
-for i in range(p.get_device_count()):
-    dev = p.get_device_info_by_index(i)
-    if dev.get('maxInputChannels') > 0:
-        print(f"Index {i}: {dev.get('name')} (입력 채널: {dev.get('maxInputChannels')})")
